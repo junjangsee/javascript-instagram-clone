3644ed703869ae61800d2ecda50ae1e316487e67
junjang <junjang.see@gmail.com>
2020-09-29 23:31:07 +0900

    [#40]feat: post exit 버튼 클릭시 profile 페이지로 이동

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,14 +1,22 @@
 const Container = require('../components/shared/Container');
 const Page = require('../lib/Page');
 const PostContainer = require('../containers/PostContainer');
+const Button = require('../components/shared/Button');
 const Image = require('../components/shared/Image');
 
 class PostPage extends Page {
-  constructor({ router = null }) {
+  constructor(router) {
     super(router);
 
     const postWrapper = new Container({ tag: 'div', className: 'post-wrapper' });
-    new Image({ parent: postWrapper, src: '../src/images/exit.png' });
+    const exitButton = new Button({
+      parent: postWrapper,
+      className: 'exit-button',
+      onClick: function () {
+        router.push('/profile');
+      },
+    });
+    new Image({ parent: exitButton, src: '../src/images/exit.png' });
     const post = new Container({ parent: postWrapper, tag: 'div', className: 'post' });
     new PostContainer(post);
   }
