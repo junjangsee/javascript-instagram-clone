8cdb15afbbd986c6659b44c8b65d6106f3340591
junjang <junjang.see@gmail.com>
2020-10-02 16:36:42 +0900

    [#42]feat: restful 방식으로 상세 페이지 구현

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -2,14 +2,23 @@ const Component = require('../lib/Component');
 const Thumbnail = require('../components/post/Thumbnail');
 const Image = require('../components/shared/Image');
 const Content = require('../components/post/Content');
+const { profileInfo } = require('../pages/mocks/datas');
 
 class PostContainer extends Component {
-  constructor({ parent, postInfo }) {
+  constructor({ parent, router }) {
     super(parent);
-    this.postInfo = postInfo[0][0];
+    this.router = router;
+
+    const datas = profileInfo.posts.map((post) => {
+      return post;
+    });
+
+    const post = datas.filter((data) => {
+      return data.id === parseInt(this.router.getRouteIndex(), 10);
+    });
 
     const thumbnail = new Thumbnail({ parent: parent });
-    this.postInfo.images.forEach((image) => {
+    post[0].images.forEach((image) => {
       new Image({
         parent: thumbnail,
         src: image.image,
@@ -17,12 +26,12 @@ class PostContainer extends Component {
     });
     new Content({
       parent: parent,
-      thumbnail: this.postInfo.thumbnail,
-      nickname: this.postInfo.nickname,
-      description: this.postInfo.description,
-      date: this.postInfo.date,
-      comments: this.postInfo.comments,
-      likes: this.postInfo.likes,
+      thumbnail: post[0].thumbnail,
+      nickname: post[0].nickname,
+      description: post[0].description,
+      date: post[0].date,
+      comments: post[0].comments,
+      likes: post[0].likes,
     });
   }
 
